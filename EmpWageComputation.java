805c1384f53eabd5d3bc46edf226c4a931af648f
vegirevathi <vegirevathi@gmail.com>
2020-05-26 11:25:33 +0530

    Adding switch case in UC4

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,22 +1,26 @@
 public class EmpWageComputation {
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+
    public static void main(String[] args) {
       System.out.println("Welcome to Employee Wage Computation");
-		int IS_FULL_TIME = 1;
-		int IS_PART_TIME = 2;
 		int EMP_RATE_PER_HOUR = 20;
 		int empHrs, empWage;
 		int empCheck = (int)Math.floor(Math.random() * 10) %3;
-		if ( empCheck == IS_FULL_TIME ){
+
+		switch ((int)empCheck){
+		case IS_FULL_TIME:
 			System.out.println("Employee is Present");
 			empHrs = 8;
-		}
-		else if (empCheck == IS_PART_TIME){
+		break;
+		case IS_PART_TIME:
          System.out.println("Employee is Part Time Present");
          empHrs = 4;
-      }
-		else {
+      break;
+		default:
 			System.out.println("Employee is Absent");
 			empHrs = 0;
+		break;
 		}
 		empWage = empHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Employee Wage:" +empWage);
