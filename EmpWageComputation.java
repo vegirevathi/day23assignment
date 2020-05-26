a1ad0e49a77ce9280d7f57ee04b900e2cadff6fa
vegirevathi <vegirevathi@gmail.com>
2020-05-26 11:55:07 +0530

    Computing Employee Wage for Multiple companies in UC8

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,39 +1,50 @@
 public class EmpWageComputation {
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
-	public static final int EMP_RATE_PER_HOUR = 20;
-   public static final int NUM_OF_WORKING_DAYS = 20;
-	public static final int MAX_WORKING_HRS = 100;
 
-   public static int computeEmpWage() {
+	private final String company;
+	private final int empRate;
+	private final int numOfDays;
+	private final int maxHrs;
+   
+	public EmpWageComputation(String company, int empRate, int numOfDays, int maxHrs){
+		this.company = company;
+		this.empRate = empRate;
+		this.numOfDays = numOfDays;
+		this.maxHrs = maxHrs;
+	}
+   public int computeEmpWage() {
       System.out.println("Welcome to Employee Wage Computation");
 		int empHrs, empWage=0, totalWage=0, totalEmpHrs=0, totalWorkingDays=0;
-		while (totalEmpHrs <= MAX_WORKING_HRS && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+		while (totalEmpHrs <= maxHrs && totalWorkingDays < numOfDays) {
 			totalWorkingDays++;
 		int empCheck = (int)Math.floor(Math.random() * 10) %3;
 		switch ((int)empCheck){
 		case IS_FULL_TIME:
-			System.out.println("Employee is Present");
+			System.out.println("Employee is Present on day " +totalWorkingDays);
 			empHrs = 8;
 		break;
 		case IS_PART_TIME:
-         System.out.println("Employee is Part Time Present");
+         System.out.println("Employee is Part Time Present on day " +totalWorkingDays);
          empHrs = 4;
       break;
 		default:
-			System.out.println("Employee is Absent");
+			System.out.println("Employee is Absent on day " +totalWorkingDays);
 			empHrs = 0;
 		break;
 		}
-		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Employee Wage:" +empWage);
+		empWage = empHrs * empRate;
+		System.out.println("Employee Wage for that day: " +empWage);
 		totalWage += empWage;
 	}
 	System.out.println("Total Wage for a month is :" +totalWage);
 	return totalWage;
 	}
 	public static void main(String[] args) {
-		computeEmpWage();
+		EmpWageComputation dMart = new EmpWageComputation("DMart", 20, 20, 10);
+		System.out.println("total emp wage for company " +dMart.company + " is " +dMart.computeEmpWage( ) );
+		EmpWageComputation BigBasket = new EmpWageComputation("BigBasket", 20, 25, 8);
+      System.out.println("total emp wage for company " +BigBasket.company + " is " +BigBasket.computeEmpWage( ) );
 	}
 }
 
