06faacdec02862aa052ce59436411e8ab41b0985
vegirevathi <vegirevathi@gmail.com>
2020-05-26 11:03:25 +0530

    Checking Employee Presence in UC1

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,5 +1,11 @@
 public class EmpWageComputation {
    public static void main(String[] args) {
       System.out.println("Welcome to Employee Wage Computation");
+		int IS_FULL_TIME = 1;
+		int empCheck = (int)Math.floor(Math.random() * 10) %2;
+		if ( empCheck == IS_FULL_TIME )
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 	}
 }
