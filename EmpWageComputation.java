b2f0713f08f96d4748bd8b8e643ef7150528c00c
vegirevathi <vegirevathi@gmail.com>
2020-05-26 11:40:32 +0530

    Adding new method for computing employee wage in UC7

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -5,7 +5,7 @@ public class EmpWageComputation {
    public static final int NUM_OF_WORKING_DAYS = 20;
 	public static final int MAX_WORKING_HRS = 100;
 
-   public static void main(String[] args) {
+   public static int computeEmpWage() {
       System.out.println("Welcome to Employee Wage Computation");
 		int empHrs, empWage=0, totalWage=0, totalEmpHrs=0, totalWorkingDays=0;
 		while (totalEmpHrs <= MAX_WORKING_HRS && totalWorkingDays < NUM_OF_WORKING_DAYS) {
@@ -30,5 +30,10 @@ public class EmpWageComputation {
 		totalWage += empWage;
 	}
 	System.out.println("Total Wage for a month is :" +totalWage);
+	return totalWage;
+	}
+	public static void main(String[] args) {
+		computeEmpWage();
 	}
 }
+
