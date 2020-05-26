67d9c00a45d40f74ef4157afdf08a60b5350cfb2
vegirevathi <vegirevathi@gmail.com>
2020-05-26 11:11:40 +0530

    Adding Employee Wage in UC2

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -2,10 +2,18 @@ public class EmpWageComputation {
    public static void main(String[] args) {
       System.out.println("Welcome to Employee Wage Computation");
 		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
+		int empHrs, empWage;
 		int empCheck = (int)Math.floor(Math.random() * 10) %2;
-		if ( empCheck == IS_FULL_TIME )
+		if ( empCheck == IS_FULL_TIME ){
 			System.out.println("Employee is Present");
-		else
+			empHrs = 8;
+		}
+		else {
 			System.out.println("Employee is Absent");
+			empHrs = 0;
+		}
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Employee Wage:" +empWage);
 	}
 }
